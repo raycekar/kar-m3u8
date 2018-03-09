8c2082af1e43ea7c67ea0913d00d62f6770c2f87
raycekar <raycekarneal@gmail.com>
2018-03-09 13:19:52 -0600

    pulls .m3u8 and .ts ext

--- scripts/jsFiles/eventPage.js
+++ scripts/jsFiles/eventPage.js
@@ -1,11 +1,16 @@
-var list = document.getElementById("linkList")
-console.log(list)
-var node = document.createElement("LI")
-node.textContent = "hello"
-list.appendChild(node)
+//var list = document.getElementById("linkList")
+//console.log(list)
+//var node = document.createElement("LI")
+//node.textContent = "hello"
+//list.appendChild(node)
+//console.log("here")
 
-chrome.devtools.network.getHAR(
-    function(harLog){
-        console.log(harLog)
-})
+chrome.webRequest.onCompleted.addListener(function (data) {
+
+    console.log(data.url)
+
+}, { urls: ["*://*/*.m3u8", "*://*/*.ts"] });
+
+
+
 
